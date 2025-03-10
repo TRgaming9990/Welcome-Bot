c7cbc0a951524b727c7ceedae4f8ff1480f762e6
TRgaming9990 <127966673+TRgaming9990@users.noreply.github.com>
2025-03-10 06:39:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,5 @@ module.exports = {
     mongoURI : "",
     statusType: 3, // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 4 = Custom
     statusContent: "A", // A = Show total users count, B = Show total servers count, C = Custom message
-    customStatus: "GlaceYT",
-    streamingURL: "https://www.twitch.tv/glaceytt"
+    customStatus: "TRgaming3955"
 };
